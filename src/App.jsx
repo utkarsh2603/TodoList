2b16c54af9b63237887e9dc82681bdc4f488c90c
utkarsh2603 <utkarsh70888@gmail.com>
2024-01-06 19:34:54 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import "./App.css";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import TodoList from "./TodoList.jsx";
 
 function App() {
